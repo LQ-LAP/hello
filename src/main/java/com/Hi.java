efd0f695377f8ca4213596b9227fd4e670be1306
lq <1812071609@qq.com>
2020-04-16 14:35:08 +0800

    second commit

---
+++ src/main/java/com/Hi.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @Author:木木弓虽
+ * @Description:
+ * @Date:Created in 14:30 2020/4/16
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
