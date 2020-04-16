cee4e6aad5179d7f505ab782e628f7c4a943ed4c
lq <1812071609@qq.com>
2020-04-16 14:12:45 +0800

    first commit

---
+++ src/main/java/com/Hello.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @Author:木木弓虽
+ * @Description:
+ * @Date:Created in 13:57 2020/4/16
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
